4301ceed0476ed57cb0cb1f8cc342ae24b6e6cfd
Daniel Pfisterer <d.pfisterer@ext.aurubis.com>
2022-10-14 14:51:17 +0200

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -73,10 +73,13 @@ set +x
 echo "";
 echo "#### STARTING THE API INSTANCE ####"
 set -x
-yarn workspace @platonist/api run stop -- --name api-"$NODE_ENV" || echo "Process was not running."
+yarn workspace @platonist/api run pm2 stop api-"$NODE_ENV" || echo "Process was not running."
 set +x
 set -x
-yarn workspace @platonist/api run serve -- --name api-"$NODE_ENV" --log pm2.log
+yarn workspace @platonist/api run pm2 start ./server.js --name api-"$NODE_ENV" --log pm2.log
+set +x
+set -x
+yarn workspace @alh/api run pm2 save
 set +x
 # END # ********************************************************************* #
 
